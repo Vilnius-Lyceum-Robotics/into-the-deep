bd4f4f3dd75cb43790bc18599ea55fc5c458026b
Adam Samulionis <adamsam@wix.com>
2024-09-07 18:52:16 +0300

    Loop Time Monitor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/LoopTimeMonitor.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.helpers;
+import java.util.TreeSet;
+import java.util.Iterator;
+
+
+public class LoopTimeMonitor {
+    private long loopStartTime;
+    private TreeSet<Long> loopTimes = new TreeSet<>();
+
+    public enum ElementSelectionType {
+        TOP_N_ELEMENTS,
+        TOP_PERCENTILE_ELEMENTS
+    }
+    
+    public void loopStart() {
+        this.loopStartTime = System.currentTimeMillis();
+    }
+
+    public void loopEnd() {
+        long loopTime = System.currentTimeMillis() - this.loopStartTime;
+        this.loopTimes.add(loopTime);
+    }
+
+    public double getAverageTime(int value, ElementSelectionType type) {
+        if (this.loopTimes.isEmpty()) {
+            return 0;
+        }
+
+        int elementCount;
+        if (type == ElementSelectionType.TOP_N_ELEMENTS) {
+            elementCount = Math.min(value, loopTimes.size());
+        } else {
+            elementCount = Math.max(1, (int) (loopTimes.size() * (value / 100.0)));
+        }
+
+        long totalTime = 0;
+        Iterator<Long> iterator = loopTimes.descendingIterator();
+
+        for(int i = 0; i < elementCount && iterator.hasNext(); i++){
+            totalTime += iterator.next();
+        }
+
+        return totalTime / (double) elementCount;
+    }
+    
+}
