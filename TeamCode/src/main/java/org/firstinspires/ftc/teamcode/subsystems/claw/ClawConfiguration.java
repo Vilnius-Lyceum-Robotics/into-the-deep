4b237407d3dedaacf28d0fb5f4c858e5f4da77ba
nojus <me@nojus.dev>
2024-09-08 13:02:41 +0300

    add config decorator to configs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/claw/ClawConfiguration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/claw/ClawConfiguration.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.subsystems.claw;
 
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
 public interface ClawConfiguration {
     double CLAW_OPEN_ANGLE = 55;
     double CLAW_CLOSED_ANGLE = 0;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/lift/LiftConfiguration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/lift/LiftConfiguration.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.subsystems.lift;
 
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
 public interface LiftConfiguration {
     int LIFT_UP_POS = 5500;
     int LIFT_DOWN_POS = 0;
