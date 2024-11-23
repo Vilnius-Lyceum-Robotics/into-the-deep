c377421c016ad04e0bd9779cea3abea6057d3cbf
Adam Samulionis <el.naujienos@gmail.com>
2024-11-23 15:40:53 +0200

    Remove get method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/utils/GlobalConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/utils/GlobalConfig.java
@@ -7,31 +7,5 @@ import java.util.Map;
 
 @Config
 public class GlobalConfig {
-    private static boolean DEBUG_MODE = false;
-
-    private static final Map<ConfigVariable, Object> defaultValueMap = new HashMap<>();
-
-    static {
-        defaultValueMap.put(ConfigVariable.DEBUG_MODE, Boolean.FALSE);
-    }
-
-    @SuppressWarnings("unchecked")
-    public static <T> T get(ConfigVariable variable) {
-        try {
-            Field field = GlobalConfig.class.getField(variable.name());
-            Object value = field.get(null);
-            if (value != null) {
-                return (T) value;
-            } else {
-                return (T) defaultValueMap.get(variable);
-            }
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            e.printStackTrace();
-            return (T) defaultValueMap.get(variable);
-        }
-    }
-
-    public enum ConfigVariable {
-        DEBUG_MODE;
-    }
+    public static boolean DEBUG_MODE = false;
 }
