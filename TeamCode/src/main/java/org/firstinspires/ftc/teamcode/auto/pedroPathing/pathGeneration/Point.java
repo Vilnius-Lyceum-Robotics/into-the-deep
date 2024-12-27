71fbf96dc25ad5fdc92e7a4ebed1a63e1e281d23
Adam0Sam <el.naujienos@gmail.com>
2024-12-27 17:34:06 +0200

    Add One More Point Class Constructor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/pedroPathing/pathGeneration/Point.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/pedroPathing/pathGeneration/Point.java
@@ -44,6 +44,10 @@ public class Point {
         setCoordinates(rOrX, thetaOrY, identifier);
     }
 
+    public Point(double X, double Y){
+        setCoordinates(X, Y, CARTESIAN);
+    }
+
     /**
      * This creates a new Point from a Pose.
      *
