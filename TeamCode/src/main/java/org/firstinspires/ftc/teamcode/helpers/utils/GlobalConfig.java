3b5337535cbbbbbf3338be9b8d557c5b2a7af419
Adam Samulionis <el.naujienos@gmail.com>
2024-12-29 13:01:25 +0200

    Add Motor Inversion (#29)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/utils/GlobalConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/utils/GlobalConfig.java
@@ -5,4 +5,5 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class GlobalConfig {
     public static boolean DEBUG_MODE = true;
+    public static boolean INVERTED_MOTORS = true;
 }
